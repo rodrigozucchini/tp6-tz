3feec5a9879fbe3659aa2e1be795e6a1de886874
unknown <rodrigozucchini1997@gmail.com>
2025-07-31 20:49:28 -0300

    cambios de modelos

---
+++ tp6-torres-zucchini/Data/Models/Conexion.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace tp6_torres_zucchini.Data.Models
+{
+    public class Conexion
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public DateTime FechaHora { get; set; } = DateTime.UtcNow;
+
+        public bool Activa { get; set; } = false;
+
+        [ForeignKey("Cliente")]
+        public int ClienteId { get; set; }
+    }
+}
